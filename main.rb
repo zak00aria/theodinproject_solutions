6f9d7d4cd248707a92a29b8e29ba58206a15ddde
Zakaria Ksira <34353281+zak00aria@users.noreply.github.com>
2024-07-21 10:32:26 +0100

    Update main.rb

--- main.rb
+++ main.rb
@@ -5,18 +5,24 @@ BOARD_SIZE = 3
 IN_ROW = 3
 board = Grid.new(BOARD_SIZE, BOARD_SIZE)
 winner = nil
-for i in 0..BOARD_SIZE * BOARD_SIZE - 1 do
+for i in 0..BOARD_SIZE * BOARD_SIZE - 1 do 
   player = i.even? ? 'X' : 'O'
   board.draw()
-  print " #{player} turn:"
   loop do
-    next_position = gets.to_i
+    print " #{player} turn:"
+    user_input = gets.strip()#strip just to remove new line at the end of the input
+    next_position = user_input.to_i
+    if next_position.to_s!=user_input#a stupid way to check if the input is a number
+      puts "Please enter a number from 0 to #{BOARD_SIZE*BOARD_SIZE-1}"
+      next
+    end
     cell = board.cells[next_position]
     if next_position >= 0 and cell and cell.value != 'X' and cell.value != 'O'
       cell.value = player
       winner = check_if_player_wins(player, cell, board, IN_ROW)?player:nil
       break
     end
+    puts "Please enter a number from 0 to #{BOARD_SIZE*BOARD_SIZE-1}"
   end
   if winner
     board.draw()
